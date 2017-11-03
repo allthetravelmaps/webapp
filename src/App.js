105baa0a52a0ccd46ce8b220dd1000a7b88fb4f0
Mike Fogel <mike@fogel.ca>
2017-11-03 18:11:10 -0300

    Integrating firebase & react-create-app, part 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+// TODO: is this importing unneeded things? (ex: messaging module)
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    // TODO: fill these in with env vars
+    // https://webpack.js.org/guides/environment-variables/
+    const config = {
+      apiKey: "<API_KEY>",
+      authDomain: "<PROJECT_ID>.firebaseapp.com",
+      databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+      storageBucket: "<BUCKET>.appspot.com",
+    };
+    firebase.initializeApp(config);
+
+    // TODO: add something like a firebase.printConfig() in here
     return (
       <div className="App">
         <header className="App-header">
