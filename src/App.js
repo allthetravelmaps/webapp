4e7075123682bb8160292de2d4c0f7476f35a368
Mike Fogel <mike@fogel.ca>
2017-11-04 11:14:49 -0300

    Adopt standard JS style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import firebase from 'firebase';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     const config = {
       apiKey: process.env.REACT_APP_API_KEY,
-      authDomain: process.env.REACT_APP_PROJECT_ID + ".firebaseapp.com",
+      authDomain: process.env.REACT_APP_PROJECT_ID + '.firebaseapp.com',
       projectId: process.env.REACT_APP_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_PROJECT_ID + ".appspot.com",
-    };
-    const fbapp = firebase.initializeApp(config, 'travelmapaddict');
+      storageBucket: process.env.REACT_APP_PROJECT_ID + '.appspot.com'
+    }
+    const fbapp = firebase.initializeApp(config, 'travelmapaddict')
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React & Firebase</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React & Firebase</h1>
         </header>
         <h4>Firebase SDK Version: {firebase.SDK_VERSION}</h4>
         <h4>Firebase Initialized App '{fbapp.name}' with config:</h4>
@@ -25,8 +25,8 @@ class App extends Component {
           {Object.keys(fbapp.options).map(key => <ul key={key}>{key}: {fbapp.options[key]}</ul>)}
         </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
