38042bbf17a72410d3882edd0d5839b6fb2fdb92
Mike Fogel <mike@fogel.ca>
2017-12-03 14:33:58 -0300

    Get mapbox map to show up in explorer

--- src/app/index.js
+++ src/app/index.js
@@ -14,16 +14,20 @@ import './index.css'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Switch>
-        <Redirect exact from='/' to='/maps/countries-of-the-world' />
-        <Route exact path='/maps' component={MapChooser} />
-        <Route path='/maps/:mapSlug' component={MapExplorer} />
-        <Route exact path='/travelers' component={TravelerDirectory} />
-        <Route path='/travelers/:username' component={TravelerProfile} />
-        <Route component={NoMatch} />
-      </Switch>
+    <div className='app'>
+      <div className='app-header'>
+        <Header />
+      </div>
+      <div className='app-main'>
+        <Switch>
+          <Redirect exact from='/' to='/maps/countries-of-the-world' />
+          <Route exact path='/maps' component={MapChooser} />
+          <Route path='/maps/:mapSlug' component={MapExplorer} />
+          <Route exact path='/travelers' component={TravelerDirectory} />
+          <Route path='/travelers/:username' component={TravelerProfile} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
     </div>
   )
 }
--- src/main/mapExplorer/index.js
+++ src/main/mapExplorer/index.js
@@ -1,10 +1,35 @@
 // @flow
 import React from 'react'
+import mapboxgl from 'mapbox-gl'
+import '../../../node_modules/mapbox-gl/dist/mapbox-gl.css'
+import './index.css'
 
-const MapExplorer = (props: { match: any }) => (
-  <div className='main main-map-explorer'>
-    Map Explorer: {props.match.params.mapSlug}
-  </div>
-)
+/* inspiration from https://github.com/alex3165/react-mapbox-gl/blob/master/src/map.tsx */
+
+class MapExplorer extends React.Component<{ match: any }, {}> {
+  container: ?HTMLDivElement
+
+  componentDidMount () {
+    const map = new mapboxgl.Map({
+      container: this.container,
+      style: 'https://openmaptiles.github.io/positron-gl-style/style-cdn.json'
+    })
+    map.addControl(new mapboxgl.FullscreenControl(), 'top-right')
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+    map.addControl(new mapboxgl.GeolocateControl({}), 'top-left')
+    map.addControl(new mapboxgl.ScaleControl({}), 'bottom-left')
+  }
+
+  render () {
+    return (
+      <div
+        className='main-map-explorer'
+        ref={x => {
+          this.container = x
+        }}
+      />
+    )
+  }
+}
 
 export default MapExplorer
