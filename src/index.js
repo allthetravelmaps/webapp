991856c02515b7b16a47b7c8146e98ddc6ed5649
Mike Fogel <mike@fogel.ca>
2017-12-06 14:21:24 -0300

    Customize bootstrap again
    Turns out the forms change layout pretty dramatically in the lowest grid
    size.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
-import 'bootstrap/dist/css/bootstrap.css'
+import './bootstrap/bootstrap.css'
 
 const rootElem = document.getElementById('root')
 if (!rootElem) throw new Error('Root element not found')
